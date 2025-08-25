13a1d6b02cdad94f8d43eaf09e3723b72eeb08bb
ismoiljon1101 <ismoiljonedu@gmail.com>
2025-08-25 22:01:57 +0900

    feat: properties page mobile style base finish

--- libs/components/mypage/Article.tsx
+++ libs/components/mypage/Article.tsx
@@ -6,7 +6,26 @@ const Article = () => {
 	const device = useDeviceDetect();
 
 	if (device === 'mobile') {
-		return <div>PROPERTY CARD</div>;
+		return (
+			<Stack className="card-config">
+				<Stack className="top">
+					<img src="/img/apartmentMain.png" alt="" />
+					<Box component={'div'} className={'date'}>
+						<Typography>July 28</Typography>
+					</Box>
+				</Stack>
+				<Stack className="bottom">
+					<Stack className="name-address">
+						<Stack className="name">
+							<Typography>Equestrian Family Home</Typography>
+						</Stack>
+						<Stack className="address">
+							<Typography>Shillymdong, Kwanak-ku, Seoul </Typography>
+						</Stack>
+					</Stack>
+				</Stack>
+			</Stack>
+		);
 	} else {
 		return (
 			<Stack className="card-config">
